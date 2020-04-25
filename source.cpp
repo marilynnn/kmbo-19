14b2ce3f607255234b1a4ac0fcb6c777f2fe96e8
marilynnn <58722837+marilynnn@users.noreply.github.com>
2020-04-25 04:58:58 +0300

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -3,34 +3,14 @@
 #include <fstream>
 
 using namespace std;
-
-void dump(const LList& l)
-{
-	for (size_t i = 0; i < l.size(); ++i, cout << " ") cout << l[i];
-}
-
-int main()
-{
-	LList l;
-	l.push_back(1);
-	l.push_front(0);
-	l.push_back(2);
-
-	cout << "Stage 1: "; dump(l); cout << endl;
-
-	l.insert_at(1, 10);
-	cout << "Stage 2: "; dump(l); cout << endl;
-
-	l.erase_at(1);
-	cout << "Stage 2: "; dump(l); cout << endl;
-
-	l.reverse();
-	cout << "Stage 3: "; dump(l); cout << endl;
-
+	LList a;
+	a.push_back(1);
+	a.push_front(0);
+	a.push_back(2);
 
 	std::ofstream test("test.txt");
 
-	test << l;
+	test << a;
 	test.close();
 
 // обработчик случаев повреждения данных
